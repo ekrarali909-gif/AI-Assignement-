680b600c926830c90713e2f91ab20d8b16a7ffbe
Ekrar Ali <ekrarali909@gmail.com>
2026-08-20 17:47:59 +0530

    MaxArea

---
+++ Maxarea.py
@@ -0,0 +1,21 @@
+def maxArea(height):
+    left = 0
+    right = len(height) - 1
+    max_water = 0
+
+    while left < right:
+        width = right - left
+        water = min(height[left], height[right]) * width
+        max_water = max(max_water, water)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_water
+
+
+# Example
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print(maxArea(height))
